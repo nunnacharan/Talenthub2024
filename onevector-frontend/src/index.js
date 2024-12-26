0ffe02b219e8f77e5be4ef9196b516da0bd3d368
Nunna Charan Naga Lakshmi Narayana <109160426+nunnacharan@users.noreply.github.com>
2024-12-26 15:53:20 +0530

    Update index.js

--- onevector-frontend/src/index.js
+++ onevector-frontend/src/index.js
@@ -3,16 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { ThemeProvider } from './ThemeContext'; // Ensure the path to ThemeContext is correct
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
+// Get the root DOM element
+const rootElement = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create the root for rendering
+const root = ReactDOM.createRoot(rootElement);
+
+// Render the app wrapped in ThemeProvider
 root.render(
   <React.StrictMode>
-    <App />
+  <Theme>
+    <ThemeProvider> {/* Wrap the entire app */}
+      <App />
+    </ThemeProvider>
+  </Theme>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// For measuring performance (optional)
 reportWebVitals();
